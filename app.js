c39c61b15a26aa41313a4b0ccd907a5b4f895bd7
CCG-Gaming-David <david@collectiblescardsandgames.com>
2026-07-10 19:21:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,32 @@ const html = `
           disableForReducedMotion: true
         });
       }, 500);
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log("test");
+})
+
+app.post('/webhook',(req, res) => {
+        res.send("POST Request Called");
+});
+
+app.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+
+  // Respond to indicate that the delivery was successfully received.
+  // Your server should respond with a 2XX response within 10 seconds of receiving a webhook delivery. If your server takes longer than that to respond, then GitHub terminates the connection and considers the delivery a failure.
+  response.status(202).send('Accepted');
+  console.log("Webhook Found!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+      
     </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
